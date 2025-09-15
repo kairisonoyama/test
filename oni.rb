f57e95454b1552db9ec5767d813d12b1526439fc
sonoyamakairi <kairi02211227@icloud.com>
2025-09-15 14:20:03 +0900

    オープニング　あり

--- oni.rb
+++ oni.rb
@@ -9,11 +9,15 @@ class GameWindow < Gosu::Window
 
     @state = :opening
     @font = Gosu::Font.new(32)
-    @title_image = Gosu::Image.new("オープニング　候補　画像.png")
-    @bgm = Gosu::Song.new("オープニング曲")
+    @title_image = Gosu::Image.new("オープニング　候補　画像.png", retro: false)
+    @bgm = Gosu::Song.new("オープニング曲.mp3")
     @bgm.play(true)
 
     @start_time = Gosu.milliseconds
+     # 選択された画像サイズに合わせてスケール計算
+    @scale_x = width / @title_image.width.to_f
+    @scale_y = height / @title_image.height.to_f
+
   end
 
   def update
@@ -33,7 +37,7 @@ class GameWindow < Gosu::Window
   end
 
   def draw_opening
-    @title_image.draw(0, 0, 0)
+    @title_image.draw(0, 0, 0, @scale_x, @scale_y)
     @font.draw_text("スペースキーでスタート", 180, 400, 1, 1.0, 1.0, Gosu::Color::YELLOW)
   end
 
@@ -46,6 +50,41 @@ end
 GameWindow.new.show
 
 
+class ScoreImageWindow < Gosu::Window
+  def initialize
+    super 640, 480
+    self.caption = "スコアに応じた画像表示"
+
+    @score = rand(3000..5000)   # 最終スコア
+
+    # 画像を読み込む
+    @low_image  = Gosu::Image.new("食中毒.png", retro: false)
+    @high_image = Gosu::Image.new("成功エンディング画像.png", retro: false)
+
+    # スコアに応じて画像を選択
+    @image = @score <= 4000 ? @low_image : @high_image
+
+    # 選択された画像サイズに合わせてスケール計算
+    @scale_x = width / @image.width.to_f
+    @scale_y = height / @image.height.to_f
+
+    #最後のメッセージの分岐
+    @message = case @score
+               when 0..4000
+                "残念ながら食中毒になりました。乙"
+               else
+                "成功しました。お腹いっぱいです。"
+               end
+  end
+
+  def draw
+    @image.draw(0, 0, 0, @scale_x, @scale_y)
+    Gosu::Font.new(32).draw_text("スコア: #{@score}\n#{@message}", 150, 150, 1, 1.0, 1.0, Gosu::Color::WHITE)
+    
+  end
+end
+
+ScoreImageWindow.new.show
 
 
 
