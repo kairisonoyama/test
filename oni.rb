b0fb205fdb6c4c2926674da86a14e0059b7a44a7
sonoyamakairi <kairi02211227@icloud.com>
2025-09-15 14:09:51 +0900

    け

--- oni.rb
+++ oni.rb
@@ -2,40 +2,50 @@
 
 require 'gosu'
 
-class ScoreImageWindow < Gosu::Window
+class GameWindow < Gosu::Window
   def initialize
     super 640, 480
-    self.caption = "スコアに応じた画像表示"
+    self.caption = "ゲームオープニング"
 
-    @score = rand(3000..5000)   # 最終スコア
+    @state = :opening
+    @font = Gosu::Font.new(32)
+    @title_image = Gosu::Image.new("オープニング　候補　画像.png")
+    @bgm = Gosu::Song.new("オープニング曲")
+    @bgm.play(true)
 
-    # 画像を読み込む
-    @low_image  = Gosu::Image.new("食中毒.png", retro: false)
-    @high_image = Gosu::Image.new("成功エンディング画像.png", retro: false)
+    @start_time = Gosu.milliseconds
+  end
 
-    # スコアに応じて画像を選択
-    @image = @score <= 4000 ? @low_image : @high_image
-
-    # 選択された画像サイズに合わせてスケール計算
-    @scale_x = width / @image.width.to_f
-    @scale_y = height / @image.height.to_f
-
-    #最後のメッセージの分岐
-    @message = case @score
-               when 0..4000
-                "残念ながら食中毒になりました。乙"
-               else
-                "成功しました。お腹いっぱいです。"
-               end
+  def update
+    if @state == :opening && Gosu.button_down?(Gosu::KB_SPACE)
+      @state = :playing
+      @bgm.stop
+    end
   end
 
   def draw
-    @image.draw(0, 0, 0, @scale_x, @scale_y)
-    Gosu::Font.new(32).draw_text("スコア: #{@score}\n#{@message}", 150, 150, 1, 1.0, 1.0, Gosu::Color::WHITE)
-    
+    case @state
+    when :opening
+      draw_opening
+    when :playing
+      draw_game
+    end
+  end
+
+  def draw_opening
+    @title_image.draw(0, 0, 0)
+    @font.draw_text("スペースキーでスタート", 180, 400, 1, 1.0, 1.0, Gosu::Color::YELLOW)
+  end
+
+  def draw_game
+    Gosu.draw_rect(0, 0, width, height, Gosu::Color::BLACK, 0)
+    @font.draw_text("いただきます！", 200, 200, 1, 1.0, 1.0, Gosu::Color::WHITE)
   end
 end
 
-ScoreImageWindow.new.show
+GameWindow.new.show
 
-class
+
+
+
+
