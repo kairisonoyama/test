060355ebf33db5b6a26f81b0f74708f5a9827aa4
sonoyamakairi <kairi02211227@icloud.com>
2025-09-15 11:56:26 +0900

    ending 音無

--- oni.rb
+++ oni.rb
@@ -7,10 +7,10 @@ class ScoreImageWindow < Gosu::Window
     super 640, 480
     self.caption = "スコアに応じた画像表示"
 
-    @score = rand(1000..3000)   # 最終スコア
+    @score = rand(3000..5000)   # 最終スコア
 
     # 画像を読み込む
-    @low_image  = Gosu::Image.new("失敗エンディング画像.png", retro: false)
+    @low_image  = Gosu::Image.new("食中毒.png", retro: false)
     @high_image = Gosu::Image.new("成功エンディング画像.png", retro: false)
 
     # スコアに応じて画像を選択
@@ -19,11 +19,20 @@ class ScoreImageWindow < Gosu::Window
     # 選択された画像サイズに合わせてスケール計算
     @scale_x = width / @image.width.to_f
     @scale_y = height / @image.height.to_f
+
+    #最後のメッセージの分岐
+    @message = case @score
+               when 0..4000
+                "残念ながら食中毒になりました。乙"
+               else
+                "成功しました。お腹いっぱいです。"
+               end
   end
 
   def draw
     @image.draw(0, 0, 0, @scale_x, @scale_y)
-    Gosu::Font.new(32).draw_text("スコア: #{@score}", 20, 20, 1, 1.0, 1.0, Gosu::Color::BLACK)
+    Gosu::Font.new(32).draw_text("スコア: #{@score}\n#{@message}", 150, 150, 1, 1.0, 1.0, Gosu::Color::WHITE)
+    
   end
 end
 
